9c295b700c3700eda94fef4ed6b96cf7c7f10fc9
ivoronline <ivoronline@gmail.com>
2021-12-16 19:21:29 +0100

    Commit

--- src/main/java/com/example/springboot_demo_store_entity_from_json_array/entities/Person.java
+++ src/main/java/com/example/springboot_demo_store_entity_from_json_array/entities/Person.java
@@ -1,13 +1,10 @@
 package com.example.springboot_demo_store_entity_from_json_array.entities;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
 public class Person {
 
